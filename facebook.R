7751f7b2f937e58ea6aa4629b946fb4f947c276f
kfcaby <kfcaby@gmail.com>
2020-09-04 14:47:11 -0400

    facebook

---
+++ facebook.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+
+# Note the file on the ISI website has some issues
+braking <- read_csv(file = "data/DriverBrakingTimes.csv")
+
+#convert to long format
+braking_long = braking %>% 
+  pivot_longer(cols = -participant,
+               names_to = "media",
+               values_to = "time")
+
+# convert participant to factor
+braking_long = braking_long %>% 
+  mutate(participant = factor(participant),
+         media = factor(media))
+
+# model using indicator encoding
+braking_long %>% 
+  lm(time ~ participant + media, data = .) %>% 
+  summary()
+
+contrasts(braking_long$media)
+
+#model using effect encoding
+contrasts(braking_long$participant) = contr.sum
+contrasts(braking_long$media) = contr.sum
+
+braking_long %>% 
+  lm(time ~ participant + media, data = .) %>% 
+  summary()
+
+braking_long %>% 
+  group_by(media) %>% 
+  summarise(mean(time))
+
+mean(braking_long$time)
